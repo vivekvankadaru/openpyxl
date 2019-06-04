2b43cd9540c333e05a00d7e00f0e5b41dd79b1f9
vankadaru vivek <vivekvankadaru.fit@gmail.com>
2019-06-04 22:31:53 +0530

    Add files via upload

--- help1.py
+++ help1.py
@@ -1,11 +1,18 @@
-import datetime as dt
+import time as dt
 import openpyxl as op
-file1=op.load_workbook("U:\\py\\help1.xlsx","rw")
+file1=op.load_workbook("U:\\py\\help1.xlsx")
 active_sheet=file1.active
 row=2
 column=2
 name=input('Enter the name')
+list_of_names=active_sheet._cells_by_col(2,2,2,10)
+for i in list_of_names:
+    print(i.value)
 while active_sheet.cell(row,column).value != None:
     if name.upper()==active_sheet.cell(row,column).value.upper():
-        active_sheet.cell(row,4)=dt.datetime.now
+        time1=dt.gmtime()
+        active_sheet.cell(row,4).value=dt.strftime("%Y-%m-%d %H:%M:%S",time1)
+    
+
     row+=1
+file1.save("U:\\py\\help1.xlsx")
